0e8eae5cca440cda339302fd2fc586dc56b5b1a7
xRayWx <mwai2782@uni.sydney.edu.au>
2020-04-30 20:50:45 +1000

    Best hand, missing straight evaluation, SERIOUS work in progress with messy code

--- src/assignments/ray/lesson_3_answers.clj
+++ src/assignments/ray/lesson_3_answers.clj
@@ -2,7 +2,7 @@
 
 ;;; Enter your answers in here.
 
-;;;; 4Clojuer question 128
+;;;; 4Clojuer question 128: Recognize Playing Cards
 
 ;; First iteration: Examining the input and output of reading first and second
 
@@ -48,7 +48,156 @@
 ;;;; Pathetic attempts on writing some tests
 
 (assert (= (read-poker-hand "DQ") {:suit :diamond :rank 10}))
-(assert (= (read-poker-hand "DQ") {:suit :blah :rank 42}))
+(assert (= (read-poker-hand "HK") {:suit :heart :rank 11}))
+
+;;;; 4Clojure question 178: Best Hand
+
+;; First iteration: Reading in a hand into suit sets and ranks sets
+
+(defn best-hand-1 [hand]
+  ;; Put both suits and ranks into sets for further processing
+  (let [[suit rank] (apply map list hand)
+        suit-set (set suit)
+        rank-set (set rank)]
+    (println suit-set)
+    (println rank-set)))
+
+;;(best-hand-1 ["DQ" "HK" "HA" "C9" "DJ"])
+
+;; Second iteration: Start processing suit frequencies, realised should not put into sets
+
+(defn best-hand-2 [hand]
+  ;; getting suits and ranks counts for initial hand determination
+  (let [[suit rank] (apply map vector hand)
+        count-suits (frequencies suit)
+        val-count-suits (vals (frequencies suit))
+        count-suits-count (count count-suits)
+        count-ranks (frequencies rank)
+        count-ranks-count (count count-ranks)]
+    ;; printing out just for testing
+    (println suit)
+    (println rank)
+    (println count-suits)
+    (println val-count-suits)
+    (println count-suits-count)
+    (println count-ranks)
+    (println count-ranks-count)
+    (cond
+      ;; conditions explained, further differentiate will be done in the next iteration
+      (= count-ranks-count 5) :flush-or-straight-flush
+      (= count-ranks-count 4) :pair
+      (= count-ranks-count 3) :two-pair-or-three-of-a-kind
+      (= count-ranks-count 2) :four-of-a-kind-or-full-house
+      (= count-suits-count 1) :flush-or-straight-flush)))
+
+;; test for sanity
+(= (best-hand-2 ["DQ" "HK" "HA" "C9" "DJ"]) :flush-or-straight-flush)
+(= (best-hand-2 ["DQ" "HQ" "HA" "C9" "DJ"]) :pair)
+(= (best-hand-2 ["DQ" "HQ" "CA" "HA" "DJ"]) :two-pair-or-three-of-a-kind)
+(= (best-hand-2 ["SQ" "DQ" "HQ" "C9" "DJ"]) :two-pair-or-three-of-a-kind)
+(= (best-hand-2 ["SK" "DK" "HK" "CK" "DJ"]) :four-of-a-kind-or-full-house)
+(= (best-hand-2 ["SJ" "DJ" "HJ" "D9" "H9"]) :four-of-a-kind-or-full-house)
+(= (best-hand-2 ["DQ" "DK" "DA" "D9" "DJ"]) :flush-or-straight-flush)
+
+;; Third iteration: All conditions laid out, excepts for straight
+
+(defn best-hand-3 [hand]
+  ;; getting suits and ranks counts for initial hand determination
+  (let [[suit rank] (apply map vector hand)
+        count-suits (frequencies suit)
+        val-count-suits (vals (frequencies suit))
+        count-suits-count (count count-suits)
+        count-ranks (frequencies rank)
+        count-ranks-count (count count-ranks)
+        val-count-ranks (vals (frequencies rank))
+        count-val-count-ranks (count (vals (frequencies rank)))]
+    ;; printing out just for testing
+    (println "")
+    (println "Hello this is a new hand")
+    (println suit)
+    (println rank)
+    (println count-suits)
+    (println val-count-suits)
+    (println count-suits-count)
+    (println count-ranks)
+    (println count-ranks-count)
+    (println "val-count-ranks" val-count-ranks)
+    (println "count-val-count-ranks" count-val-count-ranks)
+    (cond
+      ;; conditions explained, further differentiate will be done in the next iteration
+      (= count-ranks-count 5) :flush-or-straight-flush
+      (= count-ranks-count 4) :pair
+      (and (= count-ranks-count 3) (and (= count-val-count-ranks 3) (= (first val-count-ranks) 2))) :two-pair
+      (and (= count-ranks-count 3) (and (= count-val-count-ranks 3) (= (first val-count-ranks) 3))) :three-of-a-kind
+      (and (= count-ranks-count 2) (and (= count-val-count-ranks 2) (= (first val-count-ranks) 4))) :four-of-a-kind
+      (and (= count-ranks-count 2) (and (= count-val-count-ranks 2) (= (first val-count-ranks) 3))) :full-house
+      (= count-suits-count 1) :flush-or-straight-flush)))
+
+;; test for sanity
+(= (best-hand-3 ["DQ" "HK" "HA" "C9" "DJ"]) :flush-or-straight-flush)
+(= (best-hand-3 ["DQ" "HQ" "HA" "C9" "DJ"]) :pair)
+(= (best-hand-3 ["DQ" "HQ" "CA" "HA" "DJ"]) :two-pair)
+(= (best-hand-3 ["SQ" "DQ" "HQ" "C9" "DJ"]) :three-of-a-kind)
+(= (best-hand-3 ["SK" "DK" "HK" "CK" "DJ"]) :four-of-a-kind)
+(= (best-hand-3 ["SJ" "DJ" "HJ" "D9" "H9"]) :full-house)
+(= (best-hand-3 ["DQ" "DK" "DA" "D9" "DJ"]) :flush-or-straight-flush)
+
+;; Fourth iteration: All conditions mapped
+
+(defn subseq? [sub] 
+  (some #{sub} (partition 5 1 (map vector "A23456789TJQKA"))))
+
+(subseq? ["2" "3" "4" "5" "6"])
+
+(defn subseqq? [a b]
+  (some #{a} (partition (count a) 1 b)))
+
+(subseqq? [1 2 3] [1 2 3 4 5 6])
+
+(map vector "A1234")
+
+(defn best-hand-4 [hand]
+  ;; getting suits and ranks counts for initial hand determination
+  (let [[suit rank] (apply map vector hand)
+        count-suits (frequencies suit)
+        val-count-suits (vals (frequencies suit))
+        count-suits-count (count count-suits)
+        count-ranks (frequencies rank)
+        count-ranks-count (count count-ranks)
+        val-count-ranks (vals (frequencies rank))
+        count-val-count-ranks (count (vals (frequencies rank)))]
+    ;; printing out just for testing
+    (println "")
+    (println "Hello this is a new hand-4")
+    (println suit)
+    (println rank)
+    (println count-suits)
+    (println "val-count-suits" val-count-suits)
+    (println count-suits-count)
+    (println count-ranks)
+    (println count-ranks-count)
+    (println "val-count-ranks" val-count-ranks)
+    (println "count-val-count-ranks" count-val-count-ranks)
+    (cond
+      ;; conditions explained, further differentiate will be done in the next iteration
+      (= count-ranks-count 5) :straight-flush
+      (= count-ranks-count 4) :pair
+      (and (= count-ranks-count 3) (and (= count-val-count-ranks 3) (= (first val-count-ranks) 2))) :two-pair
+      (and (= count-ranks-count 3) (and (= count-val-count-ranks 3) (= (first val-count-ranks) 3))) :three-of-a-kind
+      (and (= count-ranks-count 2) (and (= count-val-count-ranks 2) (= (first val-count-ranks) 4))) :four-of-a-kind
+      (and (= count-ranks-count 2) (and (= count-val-count-ranks 2) (= (first val-count-ranks) 3))) :full-house
+      (= count-suits-count 1) :flush
+      :else :high-card)))
+
+;; test for sanity
+(= (best-hand-4 ["S2" "S3" "S4" "S5" "S6"]) :flush-or-straight-flush) ;;(= val-count-suits 5) and straight?
+(= (best-hand-4 ["DQ" "HQ" "HA" "C9" "DJ"]) :pair)
+(= (best-hand-4 ["DQ" "HQ" "CA" "HA" "DJ"]) :two-pair)
+(= (best-hand-4 ["SQ" "DQ" "HQ" "C9" "DJ"]) :three-of-a-kind)
+(= (best-hand-4 ["SK" "DK" "HK" "CK" "DJ"]) :four-of-a-kind)
+(= (best-hand-4 ["SJ" "DJ" "HJ" "D9" "H9"]) :full-house)
+(= (best-hand-4 ["DQ" "DK" "DA" "D9" "DJ"]) :flush-or-straight-flush) ;;straight?
+(= (best-hand-4 ["DQ" "DK" "DA" "D9" "DJ"]) :high-card)
 
 
    
