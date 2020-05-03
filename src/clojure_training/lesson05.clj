912c31feae2c767a8a93ec82aa8366125eca3261
Alain Picard <Dr.Alain.Picard@gmail.com>
2020-05-03 15:19:36 +1000

    Working on Lecture 5.

--- src/clojure_training/lesson05.clj
+++ src/clojure_training/lesson05.clj
@@ -3,14 +3,159 @@
    [clojure.pprint :as pprint]
    [clojure.java.io :as io]))
 
+;;; The READER.
+
+;; Lisp is strange; it has an extra step that other language don't have: the READER.
+;;
+;; Other languages:
+;; (source code text)  =>  (compiler)         => (machine executable code)
+;;  characters            intermediate AST         Binary code
+;; Lisps:
+;; (source code text)  =>  (reader)     => (compiler)     => (machine executable code)
+;;  characters         =>  lisp objects => lisp objects   =>  Binary code
+;;
+;; The READER function is what does MACROEXPANSION.
+;; The compiler receives LISP STRUCTURES as input.
+(macroexpand-1 '(-> foo (bar) (baz)))
+;; => (baz (bar foo))
+;; When then reader loads your file, and sees (-> foo (bar) (baz)),
+;; it sends (baz (bar foo)) to the compiler.
+;; That's why there is no performance penalty to using one form over another:
+;; by the time they get turned into machine code, the inputs are the SAME.
+
+;;;; READ syntax.
+;;
+;; You may have noticed a bunch of strange characters, occasionaly
+
+#_ (this form is never seen by the compiler)
+
+;; Inspect these:
+#inst "1985-04-12T23:20:50.52Z"
+#uuid "f81d4fae-7dec-11d0-a765-00a0c91e6bf6"
+
 ;;;; Java Interop
 
+;; Constructors:
 
+(new java.util.Date)
+
+;; Or, more usually, and succinctly, with trailing dot:
+(java.util.Date.)
+
+
+;; dot (.) macro
+
+;; Instance methods:
+
+(.toUpperCase "FooBar")
+
+
+(macroexpand '(.toUpperCase "FooBar"))
+(. "FooBar" toUpperCase)
+
+;; Another example:
+(.getYear (java.util.Date.))
+
+
+;;; dotdot (..) macro
+
+;; Remember this?
+(. "FooBar" toUpperCase)
+
+#_ ; Won't work
+(. "FooBar" toUpperCase toLowerCase)
+
+;; But this neat workaround exists:
+(.. "FooBar" toUpperCase toLowerCase)
+
+(macroexpand '(.. "FooBar" toUpperCase toLowerCase))
+;; => (. (. "FooBar" toUpperCase) toLowerCase)
+
+;; So .. acts as
+;; sort of a "thread first for Java interop" idea.
+;; Fails to work if you need to pass arguments to the methods
+
+(macroexpand '(.. "FooBar" toUpperCase (toLowerCase "blah")))
+
+YOU WERE HERE:
+I don't fully understand difference between .. and doto.
+
+(let [x (java.util.Date.)]
+  (. x (setYear 159))
+  x
+  )
+(.. (java.util.Date.) (setYear 159))
+
+
+;;; doto macro
+
+(doto (java.util.Date.)                 ; Even MORE fuckin' insane... :-(
+  (.setYear 137)
+  (.setMonth 0)
+  (.setDate 1)) ; macroexpand this
+
+
+;;; Static methods:
+
+(.version (java.util.UUID/randomUUID))
+
+
+;;; memfn
+
+;;; proxy
+
+;;; reify
+
+reify
+deftype
+definterface
+
+extend
+
+(deftype FooBar [name age])
+(def bob (FooBar. "Bob" 57))
+
+(.name bob)
+(->FooBar "bob" 55)
+
+
+;;; gen-class
+
+
+;;;; Show better time management libraries
+;; clj-time,
+
+
+
 ;;;; I/O
 
-;; slurp, spit
+(def path "/tmp/mystuff")
 
-;; The reader
+(spit "/tmp/mystuff" "This clojure class is too hard!\n")
+
+;; There's some options we can give;
+(spit "/tmp/mystuff" "This clojure class not so bad after all.\n" :append true)
+
+(slurp "/tmp/mystuff")
+
+
+;; Readers
+(with-open [rdr (io/reader path)]
+  (line-seq rdr)) ; Why does this blow up?
+
+(with-open [rdr (io/reader path)]
+  (into [] (line-seq rdr)))
+
+(with-open [rdr (io/reader path)]
+  (binding [*in* rdr]
+    (read-line)))
+
+(with-open [rdr (io/reader path)]
+  (read rdr))
+
+(io/make-input-stream path)
+
+
 read-string
 
 ;; The writer
@@ -27,3 +172,7 @@ io/resource
 
 
 with-out-str  with-in-str
+
+
+ns 
+import
---
+++ src/clojure_training/lesson05_answers.clj
@@ -0,0 +1,161 @@
+(ns clojure-training.lesson05-answers
+  (:require [clojure.string :as s]))
+
+
+(defn bing [term]
+  (slurp (str "https://www.bing.com?q=" term)))
+
+(defn yippy [term]
+  (slurp (str "https://www.yippy.com/search?query=" term)))
+
+(defn yahoo [term]
+  (slurp (str "https://au.search.yahoo.com/search?p=" term)))
+
+
+;; Excercise 1.
+
+(defn search-fast [term]
+  (let [p (promise)]
+    (doseq [engine [yippy yahoo bing]]
+      (future (deliver p [engine (engine term)])))
+
+    (let [[winner text] (deref p 10000 [:timeout "No results."])]
+      ;; I just took the substring so as to not print out insanely long lines.
+      [winner (subs text 0 (min 80 (.length text)))])))
+
+
+;; Excercise 2.
+(defn search-fast-2 [term & engines]
+  (let [p (promise)]
+    (doseq [engine engines]
+      (future (deliver p [engine (engine term)])))
+    (let [[winner text] (deref p 10000 [:timeout "No results."])]
+      ;; I just took the substring so as to not print out insanely long lines.
+      [winner (subs text 0 (min 80 (.length text)))])))
+
+(search-fast-2 "Lisp" bing yahoo)
+
+(search-fast-2
+ (s/escape "common lisp" {\space "&nbsp;"})
+ bing yahoo)
+
+
+;;; Excercise 3.
+
+
+
+(defn referenced-urls [s]
+  ;; a (poor) attempt at extracting some of the useful
+  ;; URLs embedded in string S.
+  (mapv second
+        (re-seq #"href=\"(http://[=?.:/\w]+)" s)))
+
+(defn get-urls [term & engines]
+  (into []
+        (flatten
+         (for [engine engines]
+           (referenced-urls (engine term))))))
+
+
+;;; Chap 10
+
+(defn fetch-1 []
+  (slurp "https://www.braveclojure.com/random-quote"))
+
+(defn words-in-sentence [s]
+  (count
+   (filter #(not= "" %)
+           (s/split s #"\s+"))))
+
+(defn count-words [quotes]
+  (reduce +
+          (map words-in-sentence quotes)))
+
+(count-words #{"Going to church doesn't make you a Christian any more than standing in a garage makes you a car.\n-- Billy Sunday \n"
+               "Make things as simple as possible, but no simpler -- Einstein."})
+
+(defn quote-count-words [n]
+  (let [results (atom {:hits 0 :quotes #{}})
+        finished? (promise)]
+    (dotimes [i n]
+      (future
+        (let [quote (fetch-1)]
+          (swap! results
+                 #(-> %
+                      (update :hits inc)
+                      (update :quotes conj quote))))
+        (when (= (:hits @results) n)
+          (deliver finished? true))))
+    (and @finished?
+         [(count-words (:quotes @results))
+          @results])))
+
+
+;;; Chap 10, ex 3.
+
+(def paladin (ref {:name "Aragorn"
+                   :hits-taken 0
+                   :danger-zone 4
+                   :max-points 40
+                   :hit-points 15}))
+
+(def wizard (ref {:name "Gandalf"
+                  :magic-potions #{:ecru :silver :vermilion}}))
+
+
+;;; The primitive operations which manipulate immutable values
+
+(defn sustain-hit [fighter hit]
+  (-> fighter
+      (update :hits-taken inc)
+      (update :hit-points - hit)))
+
+(defn quaff [fighter potion]
+  (assoc fighter :hit-points (:max-points fighter)))
+
+(defn critically-wounded? [fighter]
+  (< (:hit-points fighter) 4))
+
+(defn alive? [fighter]
+  (pos? (:hit-points fighter)))
+
+
+;; The stateful functions, which act on identities
+
+(defn hit! [fighter]
+  (alter fighter sustain-hit (rand-int 5)))
+
+(defn assist! [fighter wizard]
+  (let [[[potion] remaining] (split-at 1 (:magic-potions @wizard))]
+    (alter wizard assoc :magic-potions remaining)
+    (when potion
+      (alter fighter quaff potion))))
+
+
+(def reporter (agent nil))
+
+(defn report [_ fighter wizard]
+  (let [{:keys [hit-points name]} fighter]
+    (println name " has " hit-points " hit points left; "
+             (:name wizard) " still has " (:magic-potions wizard))))
+
+
+(defn combat! [fighter wizard]
+  (dosync
+   (hit! fighter)
+   (when (critically-wounded? @fighter)
+     (assist! fighter wizard))
+   (send reporter report @fighter @wizard)))
+
+(defn play-game []
+  (dotimes [i 4]
+    ;; We can have as many threads as we want
+    ;; playing this game simultaneously, safely.
+    (future
+      (while (alive? @paladin)
+        (combat! paladin wizard )
+        (Thread/sleep (rand-int 1000)))
+      (println "Game over!  Your paladin is dead!"))))
+
+#_
+(play-game)
